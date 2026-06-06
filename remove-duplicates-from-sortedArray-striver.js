aeaa516e410be9bc2d6138d4571eb9a102622e7e
Aaqib <drop.mohamedaaqib@gmail.com>
2026-06-06 08:30:30 +0530

    solve romove duplicates from sorted array

---
+++ remove-duplicates-from-sortedArray-striver.js
@@ -0,0 +1,97 @@
+// < !--Remove duplicates from sorted array
+
+// Given an integer array nums sorted in non - decreasing order, remove all duplicates in -place so that each unique element appears only once.
+
+
+
+// Return the number of unique elements in the array.
+
+
+// If the number of unique elements be k, then,
+
+//   Change the array nums such that the first k elements of nums contain the unique values in the order that they were present originally.
+// The remaining elements, as well as the size of the array does not matter in terms of correctness.
+// The driver code will assess correctness by printing and checking only the first k elements of the modified array.
+
+
+// An array sorted in non - decreasing order is an array where every element to the right of an element is either equal to or greater in value than that element.
+
+
+//   Example 1
+
+// Input: nums = [0, 0, 3, 3, 5, 6]
+
+// Output: 4
+
+// Explanation:
+
+// Resulting array = [0, 3, 5, 6, _, _]
+
+// There are 4 distinct elements in nums and the elements marked as _ can have any value.
+
+//   Example 2
+
+// Input: nums = [-2, 2, 4, 4, 4, 4, 5, 5]
+
+// Output: 4
+
+// Explanation:
+
+// Resulting array = [-2, 2, 4, 5, _, _, _, _]
+
+// There are 4 distinct elements in nums and the elements marked as _ can have any value.
+
+// Now your turn!
+
+// Input: nums = [-30, -30, 0, 0, 10, 20, 30, 30]
+
+// Select the possible resulting array.
+
+//   Output:
+
+// Constraints
+
+// 1 <= nums.length <= 105
+//   - 104 <= nums[i] <= 104
+// nums is sorted in non - decreasing order. -- >
+
+
+
+//   // Edgecases
+//   []-- > null
+//   [1]-- > null
+//   [1, 1, 2, 2]--> 2
+//    [2,3,4,5,6,6]--> 5  no of distinct elements in the sorted Array
+
+const nums = [-2, 2, 4, 4, 4, 4, 5, 5]
+const removeDuplicatesFromSortedArray = (nums) => {
+  // take care of the edgecases
+  if (nums.length < 1) return 0;
+  if (nums.length === 1) return 1;
+  //  create a variable to track distinct element
+  let distinctNumbers = 1;// start with 1 because my algo calculates the no of changes not no of element skipping first element .The first element is always unique.
+  // Iterate through the input array
+  for (let i = 0; i < nums.length - 1; i++) {
+    // If current element is not equal to the next element then it means it is distinct count it increase the no of distinct elements
+    if (nums[i] !== nums[i + 1]) {
+      distinctNumbers++;
+    } else if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1);
+      console.log("mutatedarray", nums);
+      i--;// move the iterator to previous index because the next index is shifted to previous index
+    }
+  }
+  // if the current element is equal to the next element then dont count it
+  return distinctNumbers;
+}
+
+
+console.log(removeDuplicatesFromSortedArray(nums));
+
+// Learnings:
+// !nums[i] evaluates not a number for eg ! 4 return false
+// splice() removes an element
+// ↓
+// all elements shift left
+// ↓
+// stay on the same index and check again
