b9c668de7efda69a4f480ef5fc23c05e7d83c773
Aaqib <drop.mohamedaaqib@gmail.com>
2026-07-17 22:20:38 +0530

    solve majority element with boyer moore algo

--- majority-element-striver.js
+++ majority-element-striver.js
@@ -87,27 +87,87 @@ const majorityElement = (nums) => {
 // Yes we can do better we can do it in 0(n)
 
 
-// Better Optimal : Hashmap approach
+// Better Approach : Hashmap approach
 const majorityElementbetter = (nums) => {
   // Create a hashmap for this storing number and their occurances
   const majority = Math.floor(nums.length / 2);
   const frequencies = {}
-  // create a variable called count
-  let count = 0;
   // Iterate through input Array
   for (let num of nums) {
-    // Whenever u come across the element again then increase the count variable
-    if (num in frequencies) count++
+    //check if number already exist in frequencies
+    if (num in frequencies) {
+      // if it exists increase the value
+      frequencies[num]++;
+    } else {
+      // or else make it 1
+      frequencies[num] = 1;
+    }
     // return the element if the element is greater than the majority
-    if (count > majority) return num;
-    // store the count as for each key which is element
-    frequencies[num] = count
+    if (frequencies[num] > majority) return num;
   }
   // fallback
   return -1;
 }
 
 console.log(majorityElementbetter(nums))
+
+
+// Time Complexity: O(n);
+// Space Complexity: O(n);
+
+
+// can we do better
+// yes we can reduce the space complexity to O(n)
+// we dont need to remember the frequencies of all the numbers we optimise that using boyer moores algorithm which relies on that if a number is more than n/2 even all the other numbers combined cannot cancel it so we can make the majority element fight
+
+
+
+// Optimal Solution - Boyer Moores Algorithm
+const boyerMoyer = (nums) => {
+  const majority = Math.floor(nums.length / 2);
+
+  // Initialise two variables count , element
+  let count = 0;
+  let element;
+  //  Iterate through the array
+  for (let num of nums) {
+    // If the count is equal to zero assign current  element to element variable and increase the count to zero
+    if (count === 0) {
+      element = num;
+      count = 1;
+    }
+    // If the same number appears increase the count
+    else if (num === element) {
+      count++;
+    }
+    // If a different number appears then decrease the count
+    else {
+      count--;
+    }
+  }
+
+
+  // verify if the element is actually the majority element
+  const majorityElementArray = nums.filter(num => num === element);
+  const majorityElementCount = majorityElementArray.length;
+  if (majorityElementCount > majority) {
+    // Return the element
+    return element
+  } else {
+    return -1;
+  }
+}
+
+console.log(boyerMoyer(nums))
+
+
+
+
+
+
+
+
+
 
 
 // Learning for your notes
@@ -151,4 +211,12 @@ console.log(majorityElementbetter(nums))
 // Avoid duplicate combinations
 // One-line interview memory trick
 
-// j = i + 1 is for avoiding duplicate combinations; j = 0 is for counting complete information.
+// // j = i + 1 is for avoiding duplicate combinations; j = 0 is for counting complete information.
+// Frequency pattern
+// For each element
+
+//     If element exists
+//         Increment frequency
+
+//     Else
+//         Initialize frequency to 1
